8f1949acf9775221d34b820b9d69dbd48a65ac29
titofebles74 <titofebles@gmail.com>
2021-02-11 18:23:59 -0600

    "Primera version"

--- adminsite/settings.py
+++ adminsite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'meriland',
     'bootstrap4',
     'rest_framework',
-    #'rest_framework.authtoken',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#REST_FRAMEWORK = {
-#    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#    'PAGE_SIZE': 10
-#}
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 '''
 REST_FRAMEWORK = {
