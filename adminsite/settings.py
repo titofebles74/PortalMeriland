cf4d778f14e053a360a411ad00cb143bd2b7cd3e
titofebles74 <titofebles@gmail.com>
2021-03-18 17:21:22 -0600

    "Cambios base Mongodb"

--- adminsite/settings.py
+++ adminsite/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!g3=b0*z%4zfjw2$6j4(jhe%=5ze0$!(&3tel2ibl($5i0a_^)'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,12 +23,6 @@ SECRET_KEY = '!g3=b0*z%4zfjw2$6j4(jhe%=5ze0$!(&3tel2ibl($5i0a_^)'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
-
-#recaptcha
-RECAPTCHA_SECRET_KEY = '6LeECWIaAAAAAHAY4N0hxyT5VGMQs8COhrz_stkh'
-RECAPTCHA_PUBLIC_KEY = '6LeECWIaAAAAAEIpB5NMM-HSubTXs4z2N63VKHEs'
-RECAPTCHA_PRIVATE_KEY = '6LeECWIaAAAAAEIpB5NMM-HSubTXs4z2N63VKHEs'
 
 
 # Application definition
